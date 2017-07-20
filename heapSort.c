ff64f6d43d1b7fbf0af52f3b3d4a9671e819d4b7
Dheeraj <dheeraj98reddy@gmail.com>
2017-07-20 13:09:56 +0530

    Completed heap sort

---
+++ heapSort.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct MaxHeap
+{
+	int size;
+	int* array;
+};
+
+void swap(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void heapify(struct MaxHeap* maxHeap, int idx)
+{
+	int largest = idx;
+	int left = (idx << 1) + 1;
+	int right = (idx + 1) << 1;
+
+	if( left < maxHeap->size && 
+			maxHeap->array[left] > maxHeap->array[largest])	
+		largest = left;
+
+	if( right < maxHeap->size && 
+			maxHeap->array[right] > maxHeap->array[largest])
+		largest = right;
+
+	if(largest != idx)
+	{
+		swap(&maxHeap->array[idx], &maxHeap->array[largest]);
+		heapify(maxHeap, largest);
+	}
+}
+
+struct MaxHeap* createHeap(int *array, int size)
+{
+	int i;
+	struct MaxHeap* maxHeap = (struct MaxHeap*) malloc(sizeof(struct MaxHeap));
+	maxHeap->array = array;
+	maxHeap->size = size;
+
+	for(i = (maxHeap->size - 2) / 2; i >= 0; i--)
+		heapify(maxHeap, i);
+
+	return maxHeap;
+}
+
+void heapSort(int* array, int size)
+{
+	struct MaxHeap* maxHeap = createHeap(array, size);
+	while(maxHeap->size > 1)
+	{
+		swap(maxHeap->array, maxHeap->array + maxHeap->size - 1);
+		--maxHeap->size;
+
+		heapify(maxHeap, 0);
+	}
+}
+
+int main()
+{
+	srand(time(NULL));
+	int size, *arr, i;
+	scanf("%d", &size);
+	arr = (int*) malloc(size * sizeof(int));
+
+	for(i = 0; i < size; i++)
+		*(arr + i) = rand() % size;
+	
+	heapSort(arr, size);
+	
+	for(i = 0; i < size; i++)
+		printf("%d\n", *(arr + i));
+
+	return 0;
+}
