0892b087c122cfc8fa8ff5bf32d9b6050004f285
Dheeraj R Reddy <dheeraj98reddy@gmail.com>
2018-04-19 03:53:47 +0530

    Fix Warshall, no need for multiple matrices

--- warshall.c
+++ warshall.c
@@ -4,32 +4,17 @@
 #include "graph.h"
 
 int** warshall(int **mat, int vertices) {
-	int ***space = (int***)malloc((vertices+1)*sizeof(int**));
-	space[0] = mat;
+	for(int k = 0; k < vertices; k++) {
+		for(int i = 0; i < vertices; i++) {
+			for(int j = 0; j < vertices; j++) {
+				mat[i][j] = mat[i][j] || (mat[i][k] && mat[k][j]);
+			}
+		}
+	}
 
-	for(int k = 1; k <= vertices; k++) {
-		space[k] = (int**) malloc(vertices*sizeof(int*));
-		for(int i = 0; i < vertices; i++) {
-			space[k][i] = (int*) malloc(vertices*sizeof(int));
-			for(int j = 0; j < vertices; j++){
-				space[k][i][j] = 0;
-			}
-		}
-	}
-	
-	printf("Space alloc'd\n");
-	for(int k = 1; k <= vertices; k++) {
-		for(int i = 0; i < vertices; i++) {
-			for(int j = 0; j < vertices; j++){
-				space[k][i][j] = space[k-1][i][j] || (space[k-1][i][k-1] && space[k-1][k-1][j]);
-			}
-		}
-	}
-	
-	int **m = space[vertices];
 	for(unsigned int i = 0; i < vertices; i++) {
 		for(unsigned int j = 0; j < vertices; j++) {
-			printf("%d\t", m[i][j]);
+			printf("%d\t", mat[i][j]);
 		}
 		printf("\n");
 	}
@@ -43,7 +28,7 @@ int main() {
 	addEdge(g, 3, 0, 1);
 	addEdge(g, 3, 2, 1);
 	displayGraph(g);
-	
+
 	int **mat = getBooleanMatrix(g);
 
 	warshall(mat, g->totalVertices);
